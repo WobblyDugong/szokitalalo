54999c599e6f765e6ad11900a6c23c8de081dbe9
BerenyiPeti <berenyipeti01@gmail.com>
2022-03-24 21:27:47 +0100

    összehasonlító metódus 2. verzió

--- szokitalaloJatek/szokitalaloJatek1/src/szokitalalojatek1/SzokitalaloJatek1.java
+++ szokitalaloJatek/szokitalaloJatek1/src/szokitalalojatek1/SzokitalaloJatek1.java
@@ -18,28 +18,47 @@ public class SzokitalaloJatek1 {
     }
 
     private static void jobetuE() {
-        String tipp = "ab";
-        String szo = "aa";
+        String tipp = "őr";
+        String szo = "őr";
         char[] tippDarabolt = tipp.toCharArray();
         char[] szoDarabolt = szo.toCharArray();
         char[] joBetuk = new char[2];
         char[] rosszBetuk = new char[2];
-        System.out.println("jobetük:"+joBetuk.length);
+        //char[] joHelyenVannak = new char[2];
         //String darabolt1 = darabolt[0], darabolt2 = darabolt[1];
         int index = 0;
+        int joBetuIndex = 0;
+        int rosszBetuIndex = 0;
+        //int joHelyenVannakIndex = 0;
+        
         while (index < tippDarabolt.length) {
             int kindex = 0;
             while (kindex < szoDarabolt.length && !(tippDarabolt[index] == szoDarabolt[kindex])) {
                 kindex++;
             }
             boolean talalt = kindex < szoDarabolt.length;
+            boolean joHelyenVan = index == kindex && talalt;
             if (talalt) {
-                joBetuk[index] = tippDarabolt[index];
+                joBetuk[index/*joBetuk.length+joBetuIndex-2*/] = tippDarabolt[index];
+                //joBetuIndex++;
             } else {
-                rosszBetuk[index] = tippDarabolt[index];
+                rosszBetuk[rosszBetuk.length+rosszBetuIndex-2] = tippDarabolt[index];
+                rosszBetuIndex++;
             }
+            
+            /*if (talalt) {
+                for (int i = 0; i < tippDarabolt.length && !(tippDarabolt[i] == szoDarabolt[index]); i++) {
+                    System.out.println(i);
+                }
+              
+                /*joHelyenVannak[joHelyenVannak.length+joHelyenVannakIndex-2] = tippDarabolt[index];
+                joHelyenVannakIndex++;
+            }*/
+            
             index++;
         }
+        
+        
 
         //boolean talalt = index < karakterek.length;
         //System.out.println(talalt);
@@ -47,14 +66,18 @@ public class SzokitalaloJatek1 {
         for (int i = 0; i < joBetuk.length; i++) {
             System.out.println(joBetuk[i]);
         }
+       
+        /*System.out.println("Jó helyen van:");
+        for (int i = 0; i < joHelyenVannak.length; i++) {
+            System.out.println(joHelyenVannak);
+
+        }*/
+        
         System.out.println("Rossz betűk:");
         for (int i = 0; i < rosszBetuk.length; i++) {
             System.out.println(rosszBetuk[i]);
         }
-        
-        
-        
-        
+
     }
 
 }
